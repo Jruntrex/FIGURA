5095974348a66ff9e44631eb5a6f6658aab9266a
Serhii Hodis <serghod@icloud.com>
2026-02-20 10:10:16 +0200

    Contact Page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,17 +3,16 @@ import { LanguageProvider } from '@/app/context/LanguageContext';
 import { HelmetProvider } from 'react-helmet-async';
 import LandingPage from './components/LandingPage';
 import ContactPage from './components/ContactPage';
-import UnderConstruction from './components/UnderConstruction';
+
+import { ScrollToTop } from '@/components/utils/ScrollToTop';
 
 const AppRoutes = () => {
-  const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route
         path="/contact"
-        element={isLocalhost ? <ContactPage /> : <UnderConstruction />}
+        element={<ContactPage />}
       />
     </Routes>
   );
@@ -24,6 +23,7 @@ const App = () => {
     <LanguageProvider>
       <HelmetProvider>
         <BrowserRouter>
+          <ScrollToTop />
           <AppRoutes />
         </BrowserRouter>
       </HelmetProvider>
